72ed38e4340f8ad2c722f996db66b4dafb97da52
kakashizc <359005442@qq.com>
2021-04-09 17:49:18 +0800

    tijiao

---
+++ App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+
+		setTimeout(() => {
+			uni.setTabBarBadge({
+				// index: 1,
+				// text: '31' //设置一个数字提醒
+			});
+			uni.showTabBarRedDot({
+				//index: 3 //设置一个大圆点
+			});
+		}, 1000);
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style>
+/*每个页面公共css */ 
+</style>
---
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -0,0 +1,53 @@
+<template>
+	 <view>
+	        <swiper class="swiper" :indicator-dots="false" :autoplay="true" :interval="3000" :circular="true">
+	        	<swiper-item v-for="item in db.data">
+	        		<image :src="item.image" mode="aspectFill"></image>
+	        	</swiper-item>
+	        </swiper>
+	        
+	</view>
+</template>
+
+<script>
+export default {
+		data() {
+	        return {
+	            db: [],
+	        }
+	    },
+		onLoad() {
+			this.indexData()
+		},
+	    methods: {
+	        indexData() {
+				uni.request({
+				    url: 'https://wx.zhoujiasong.top/api/index/getBanner', 
+				    success: (res) => {
+				        console.log(res.data.data);
+						this.db = res.data
+				    }
+				});
+	        },
+	    }
+};
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+	.swiper {
+		height: 400rpx;
+		padding: 20rpx;
+		box-sizing: border-box;
+	}
+
+	.swiper image {
+		width: 100%;
+		height: 100%;
+		border-radius: 20rpx;
+	}
+</style>
